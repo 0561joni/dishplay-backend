0694ebdef180f9c36fc1448f095a0070af5ff296
0561joni <jonathanzuecker@web.de>
2025-08-07 13:17:54 +0200

    correct import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends, UploadFile, File, status
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import JSONResponse
